23d9b19dff5f2698af51e503f92a89cfef09cae0
Linda Ogbevoen <lindaogbevoen@gmail.com>
2018-02-26 21:58:25 +0000

    added devise gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  #Added per Devise instructions
+  config.action_mailer.default_url_options = { host: 'localhost:3030' } 
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -25,7 +28,7 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
+  
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
