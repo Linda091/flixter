d0320efb5b93d2e5c304eb0644a98602292c7ff5
Linda Ogbevoen <lindaogbevoen@gmail.com>
2018-03-08 18:01:26 +0000

    added the carrierwave gem

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -41,7 +41,7 @@ class Instructor::CoursesController < ApplicationController
 
 
   def course_params
-    params.require(:course).permit(:title, :description, :cost)
+    params.require(:course).permit(:image, :title, :description, :cost)
   end
 
 end
