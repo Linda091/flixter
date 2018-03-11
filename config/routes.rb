7ae20d2732a0edfd02615ee460d38c26aa657154
Linda Ogbevoen <lindaogbevoen@gmail.com>
2018-03-11 19:17:03 +0000

    added student enrollemt feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
-  resources :courses, only: [:index, :show]
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end
+
   resources :lessons, only: [:show]
+
   namespace :instructor do
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create, :show, :edit, :destroy]
     end
     
     resources :courses, only: [:new, :create, :show, :edit, :destroy] do
-     resources :sections, only: [:new, :create, :show, :edit, :destroy] do
-      
-     end
+     resources :sections, only: [:new, :create, :show, :edit, :destroy]
     end
   end
 end
