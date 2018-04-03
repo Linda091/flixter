f0f7b9f99863aa490b175b3fe6f5dfa02e814bfb
Linda Ogbevoen <lindaogbevoen@gmail.com>
2018-04-03 15:55:27 +0000

    adding drag and drop feature for sections

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -6,4 +6,9 @@ class Lesson < ApplicationRecord
   include RankedModel
   ranks :row_order, with_same: :section_id
 
+  def next_lesson
+    lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
+    return lesson
+  end
+
 end
