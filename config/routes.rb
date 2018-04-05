dc1cf6f2837007be507fb706abafeec0ea174eb7
Linda Ogbevoen <lindaogbevoen@gmail.com>
2018-04-05 00:47:20 +0000

    added ability to reorder sections

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   namespace :instructor do
     resources :lessons, only: [:update]
+    resources :sections, only: [:update]
+    
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create, :show, :edit, :destroy]
     end
